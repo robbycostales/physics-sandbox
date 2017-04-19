4f0092505e1cad0a7a284a8a4c43517b57246788
Robby Costales <rcostales15@simons-rock.edu>
2017-04-18 23:43:56 -0400

    almost done

--- src/StuffLab.java
+++ src/StuffLab.java
@@ -2,14 +2,15 @@ import java.awt.Color;
 import java.util.Random;
 
 public class StuffLab {
+    // CONSTANT DECLARATIONS
+    public static final int ROWS = 150;
+    public static final int COLS = 150;
+
     public static void main(String[] args) {
-        StuffLab lab = new StuffLab(120, 80);
+        StuffLab lab = new StuffLab(ROWS, COLS);
         lab.run();
     }
 
-    // CONSTANT DECLARATIONS
-    public static final int ROWS = 120;
-    public static final int COLS = 80;
     // PARTICLE CONSTANTS
     public static final int EMPTY = 0;
     public static final int SAND = 1;
@@ -17,6 +18,10 @@ public class StuffLab {
     public static final int GAS = 3;
     public static final int WALL = 4;
     public static final int WALLY = 5;
+    public static final int BRICK = 6;
+    public static final int MISSILE = 7;
+    public static final int SLUDGE = 8;
+    public static final int LAVA = 9;
 
 
     // FIELDS (can add more)
@@ -27,19 +32,27 @@ public class StuffLab {
     public StuffLab(int numRows, int numCols)
     {
         String[] names;
-        names = new String[6]; // MUST CHANGE LENGTH IF YOU WANT TO ADD SOMETHING !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        names = new String[10]; // MUST CHANGE LENGTH IF YOU WANT TO ADD SOMETHING !!!!!!!!!!!!!!!!!!!!!!!!!!!!
         names[EMPTY] = "Empty";
         names[SAND] = "Sand ";
         names[WATER] = "Water";
-        names[GAS] = "Weird Gas";
+        names[GAS] = "Gas";
         names[WALL] = "Wall (.)";
         names[WALLY] = "Wall (+)";
+        names[BRICK] = "Brick";
+        names[MISSILE] = "Missile";
+        names[SLUDGE] = "Sludge";
+        names[LAVA] = "Lava";
         display = new StuffDisplay("Falling Sand", numRows, numCols, names);
         grid = new int[numRows][numCols];
     }
 
     // CALLED ON CLICK OF SCREEN
     private void locationClicked(int row, int col, int tool) {
+        if (grid[row][col] != EMPTY && grid[row][col] != WATER && grid[row][col] != GAS){ // if neither empty nor water nor gas
+            return;
+        }
+
         if (tool == WALLY && row != 0 && row != ROWS-1 && col != 0 && col != COLS -1){
             grid[row][col] = tool;
             grid[row+1][col] = tool;
@@ -56,8 +69,8 @@ public class StuffLab {
 
     // GRID -> DISPLAY
     public void updateDisplay() {
-        for (int row=0; row < 120; row++) {
-            for (int column=0; column < 80; column++) {
+        for (int row=0; row < ROWS; row++) {
+            for (int column=0; column < COLS; column++) {
                 if (grid[row][column]==(EMPTY)) {display.setColor(row, column, Color.black);}
                 else if (grid[row][column]==(SAND)){display.setColor(row, column, Color.yellow);}
                 else if (grid[row][column]==(WATER)){display.setColor(row, column, Color.blue);}
@@ -72,187 +85,267 @@ public class StuffLab {
 
     //called repeatedly.
     //causes one random particle to maybe do something.
-    public void step(int x, int y)
-    {
+    public void step(int x, int y) {
         // NOTE TO SELF: Remember that we are in form grid[y][x]
 
         // if sand
-        if (grid[y][x] == SAND)
-        {
-            // if at bottom, do nothing
-            if (y == ROWS - 1) {
-                return;
-            }
-            // if nothing below sand
-            if (grid[y + 1][x] == EMPTY) {
-                // sand falls
-                grid[y + 1][x] = SAND;
-                grid[y][x] = EMPTY;
-                return;
-            }
-            // if water below sand
-            if (grid[y + 1][x] == WATER) {
-                // sand falls through water
-                grid[y + 1][x] = SAND;
-                grid[y][x] = WATER;
-                return;
-            }
-            else{
-                return;
-            }
-        }
 
-
-        // if water
-        if (grid[y][x] == WATER)
-        {
-            // if at bottom, do nothing
-            if (y==ROWS-1)
-            {
-                return;
-            }
-            // if nothing below water
-            if (grid[y + 1][x] == EMPTY)
-            {
-                // water falls
-                grid[y + 1][x] = WATER;
-                grid[y][x] = EMPTY;
-                return;
-            }
-
-            // if gas below water
-            if (grid[y + 1][x] == GAS)
-            {
-                // water falls
-                grid[y + 1][x] = WATER;
-                grid[y][x] = GAS;
-                return;
-            }
-
-            // if something below water
-            if (grid[y + 1][x] != EMPTY)
-            {
-                // if on sides, move away
-                if ( x == 0 && grid[y][x+1] == EMPTY){
+        switch (grid[y][x]) {
+            case SAND:
+                // if at bottom, do nothing
+                if (y == ROWS - 1) {
+                    return;
+                }
+                // if nothing below sand
+                if (grid[y + 1][x] == EMPTY) {
+                    // sand falls
+                    grid[y + 1][x] = SAND;
                     grid[y][x] = EMPTY;
-                    grid[y][x+1] = WATER;
                     return;
                 }
-                if ( x == COLS-1 && grid[y][x-1] == EMPTY){
+
+                // if water below sand
+                if (grid[y + 1][x] == WATER) {
+                    // sand falls through water
+                    grid[y + 1][x] = SAND;
+                    grid[y][x] = WATER;
+                    return;
+                }
+                break;
+
+            case WATER:
+                // if at bottom, do nothing
+                if (y == ROWS - 1) {
+                    return;
+                }
+                // if nothing below water
+                if (grid[y + 1][x] == EMPTY) {
+                    // water falls
+                    grid[y + 1][x] = WATER;
                     grid[y][x] = EMPTY;
-                    grid[y][x-1] = WATER;
                     return;
                 }
-                // if on sides and trapped, stay
-                if (x == 0 || x == COLS -1){
+
+                // if gas below water
+                if (grid[y + 1][x] == GAS) {
+                    // water falls
+                    grid[y + 1][x] = WATER;
+                    grid[y][x] = GAS;
                     return;
                 }
-                // if otherwise locked in, stay
-                if ( grid[y][x+1] != EMPTY && grid[y][x-1] != EMPTY){
+
+                // if lava below water
+                if (grid[y + 1][x] == LAVA) {
+                    // water falls
+                    grid[y][x] = GAS;
                     return;
                 }
-                // else, choose randomly between left and right movement
-                Random side = new Random();
-                int choice = side.nextInt(1);
-                // if 0, preference towards leftward movement
-                if (choice == 0){
-                    if ( grid[y][x-1] == EMPTY ){
-                        grid[y][x-1] = WATER;
+
+                // if something below water
+                if (grid[y + 1][x] != EMPTY) {
+                    // if on sides, move away
+                    if (x == 0 && grid[y][x + 1] == EMPTY) {
                         grid[y][x] = EMPTY;
+                        grid[y][x + 1] = WATER;
                         return;
                     }
-                    else{
-                        grid[y][x+1] = WATER;
+                    if (x == COLS - 1 && grid[y][x - 1] == EMPTY) {
                         grid[y][x] = EMPTY;
+                        grid[y][x - 1] = WATER;
                         return;
+                    }
+                    // if on sides and trapped, stay
+                    if (x == 0 || x == COLS - 1) {
+                        return;
+                    }
+                    // if otherwise locked in, stay
+                    if (grid[y][x + 1] != EMPTY && grid[y][x - 1] != EMPTY) {
+                        return;
+                    }
+                    // else, choose randomly between left and right movement
+                    Random side = new Random();
+                    int choice = side.nextInt(2);
+                    // if 0, preference towards leftward movement
+                    if (choice == 0) {
+                        if (grid[y][x - 1] == EMPTY) {
+                            grid[y][x - 1] = WATER;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x + 1] = WATER;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
+                    }
+                    // if 1, preference towards rightward movement
+                    else {
+                        if (grid[y][x + 1] == EMPTY) {
+                            grid[y][x + 1] = WATER;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x - 1] = WATER;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
                     }
                 }
-                // if 1, preference towards rightward movement
-                else{
-                    if ( grid[y][x+1] == EMPTY ){
-                        grid[y][x+1] = WATER;
-                        grid[y][x] = EMPTY;
-                        return;
-                    }
-                    else{
-                        grid[y][x-1] = WATER;
-                        grid[y][x] = EMPTY;
-                        return;
-                    }
+                break;
+
+            case GAS:
+                // if at top, do nothing
+                if (y == 0) {
+                    return;
                 }
-            }
-        }
-
-        // if gas
-        if (grid[y][x] == GAS)
-        {
-            // if at top, do nothing
-            if (y==0)
-            {
-                return;
-            }
-            // if nothing above gas
-            if (grid[y - 1][x] == EMPTY)
-            {
-                // gas rises
-                grid[y - 1][x] = GAS;
-                grid[y][x] = EMPTY;
-                return;
-            }
-
-            // if something above gas
-            if (grid[y - 1][x] != EMPTY)
-            {
-                // if on sides, move away
-                if ( x == 0 && grid[y][x+1] == EMPTY){
+                // if nothing above gas
+                if (grid[y - 1][x] == EMPTY) {
+                    // gas rises
+                    grid[y - 1][x] = GAS;
                     grid[y][x] = EMPTY;
-                    grid[y][x+1] = GAS;
-                    return;
-                }
-                if ( x == COLS-1 && grid[y][x-1] == EMPTY){
-                    grid[y][x] = EMPTY;
-                    grid[y][x-1] = GAS;
-                    return;
-                }
-                // if on sides and trapped, stay
-                if (x == 0 || x == COLS -1){
-                    return;
-                }
-                // if otherwise locked in, stay
-                if ( grid[y][x+1] != EMPTY && grid[y][x-1] != EMPTY){
                     return;
                 }
 
-                // else, choose randomly between left and right movement
+                // if something above gas
+                if (grid[y - 1][x] != EMPTY) {
+                    // if on sides, move away
+                    if (x == 0 && grid[y][x + 1] == EMPTY) {
+                        grid[y][x] = EMPTY;
+                        grid[y][x + 1] = GAS;
+                        return;
+                    }
+                    if (x == COLS - 1 && grid[y][x - 1] == EMPTY) {
+                        grid[y][x] = EMPTY;
+                        grid[y][x - 1] = GAS;
+                        return;
+                    }
+                    // if on sides and trapped, stay
+                    if (x == 0 || x == COLS - 1) {
+                        return;
+                    }
+                    // if otherwise locked in, stay
+                    if (grid[y][x + 1] != EMPTY && grid[y][x - 1] != EMPTY) {
+                        return;
+                    }
+
+                    // else, choose randomly between left and right movement
+                    Random side1 = new Random();
+                    int doice = side1.nextInt(2);
+                    // if 0, preference towards leftward movement
+                    if (doice == 0) {
+                        if (grid[y][x - 1] == EMPTY) {
+                            grid[y][x - 1] = GAS;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x + 1] = GAS;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
+                    }
+                    // if 1, preference towards rightward movement
+                    else {
+                        if (grid[y][x + 1] == EMPTY) {
+                            grid[y][x + 1] = GAS;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x - 1] = GAS;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
+                    }
+                }
+                break;
+            case WALL:
+                break;
+            case WALLY:
+                break;
+            case BRICK:
+                break;
+            case MISSILE:
+                break;
+            case SLUDGE:
+                break;
+            case LAVA:
                 Random side1 = new Random();
-                int doice = side1.nextInt(1);
-                // if 0, preference towards leftward movement
-                if (doice == 0){
-                    if ( grid[y][x-1] == EMPTY ){
-                        grid[y][x-1] = GAS;
+                int choice = side1.nextInt(4);
+                if (choice == 0 || choice == 1 || choice == 2){
+                    return;
+                }
+                // if at bottom, do nothing
+                if (y == ROWS - 1) {
+                    return;
+                }
+                // if nothing below water
+                if (grid[y + 1][x] == EMPTY) {
+                    // water falls
+                    grid[y + 1][x] = LAVA;
+                    grid[y][x] = EMPTY;
+                    return;
+                }
+
+                // if gas below water
+                if (grid[y + 1][x] == GAS) {
+                    // water falls
+                    grid[y + 1][x] = LAVA;
+                    grid[y][x] = GAS;
+                    return;
+                }
+
+                // if something below water
+                if (grid[y + 1][x] != EMPTY) {
+                    // if on sides, move away
+                    if (x == 0 && grid[y][x + 1] == EMPTY) {
                         grid[y][x] = EMPTY;
+                        grid[y][x + 1] = LAVA;
                         return;
                     }
-                    else{
-                        grid[y][x+1] = GAS;
+                    if (x == COLS - 1 && grid[y][x - 1] == EMPTY) {
                         grid[y][x] = EMPTY;
+                        grid[y][x - 1] = LAVA;
                         return;
+                    }
+                    // if on sides and trapped, stay
+                    if (x == 0 || x == COLS - 1) {
+                        return;
+                    }
+                    // if otherwise locked in, stay
+                    if (grid[y][x + 1] != EMPTY && grid[y][x - 1] != EMPTY) {
+                        return;
+                    }
+                    // else, choose randomly between left and right movement
+                    Random side = new Random();
+                    choice = side.nextInt(1);
+                    // if 0, preference towards leftward movement
+                    if (choice == 0) {
+                        if (grid[y][x - 1] == EMPTY) {
+                            grid[y][x - 1] = LAVA;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x + 1] = LAVA;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
+                    }
+                    // if 1, preference towards rightward movement
+                    else {
+                        if (grid[y][x + 1] == EMPTY) {
+                            grid[y][x + 1] = LAVA;
+                            grid[y][x] = EMPTY;
+                            return;
+                        } else {
+                            grid[y][x - 1] = LAVA;
+                            grid[y][x] = EMPTY;
+                            return;
+                        }
                     }
                 }
-                // if 1, preference towards rightward movement
-                else{
-                    if ( grid[y][x+1] == EMPTY ){
-                        grid[y][x+1] = GAS;
-                        grid[y][x] = EMPTY;
-                        return;
-                    }
-                    else{
-                        grid[y][x-1] = GAS;
-                        grid[y][x] = EMPTY;
-                        return;
-                    }
-                }
-            }
+                break;
+            case EMPTY:
+                break;
+            default:
+                break;
         }
     }
 
